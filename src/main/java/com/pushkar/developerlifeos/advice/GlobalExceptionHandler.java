8512d3e86b09e1f744c2d4e82435ea3cdbc0f6f0
Prajapatipushkar <kppusakar2018@gmail.com>
2026-07-21 16:16:22 +0530

    Handle validation exception globally

--- src/main/java/com/pushkar/developerlifeos/advice/GlobalExceptionHandler.java
+++ src/main/java/com/pushkar/developerlifeos/advice/GlobalExceptionHandler.java
@@ -3,19 +3,45 @@ package com.pushkar.developerlifeos.advice;
 import com.pushkar.developerlifeos.exception.TaskNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(TaskNotFoundException.class)
-    public ResponseEntity<String> handleTaskNotFound(TaskNotFoundException ex) {
+    public ResponseEntity<String> handleTaskNotFound(
+            TaskNotFoundException ex){
 
         return new ResponseEntity<>(
                 ex.getMessage(),
-                HttpStatus.NOT_FOUND
-        );
+                HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>>
+    handleValidationExceptions(
+            MethodArgumentNotValidException ex){
+
+        Map<String,String> errors = new HashMap<>();
+
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error -> {
+
+                    errors.put(
+                            error.getField(),
+                            error.getDefaultMessage());
+
+                });
+
+        return new ResponseEntity<>(
+                errors,
+                HttpStatus.BAD_REQUEST);
     }
 
 }
